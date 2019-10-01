daf306c5bded1078549c77678623148ba3a802be
Guilherme Augusto Alves <41652807+alvesmog@users.noreply.github.com>
2019-10-01 14:22:31 -0300

    Pedido

--- src/classes/Pedido.java
+++ src/classes/Pedido.java
@@ -7,10 +7,34 @@ import utils.Utils;
 
 public class Pedido {
 		
-	Pedido(Cliente cl, Comida c, Bebida b){
+	Pedido(Cliente cl, Comida c1, Comida c2, Comida c3, Bebida b){
 		
 	}
 	
+	Pedido(Cliente cl, Comida c1, Comida c2,  Bebida b){
+		
+	}
+	
+	Pedido(Cliente cl, Comida c1, Bebida b){
+		
+	}
+	
+	Pedido(Cliente cl,  Bebida b){
+		
+	}
+	
+	Pedido(Cliente cl, Comida c1, Comida c2, Comida c){
+		
+	}
+	
+	Pedido(Cliente cl, Comida c1, Comida c2){
+		
+	}
+	
+	Pedido(Cliente cl, Comida c1){
+		
+	}
+		
 	Pedido(){
 		
 	}
@@ -19,13 +43,15 @@ public class Pedido {
 		
 		Utils.limparTela();
 		
+		int construtor = 0;
+		
 		//Obtendo o cliente		
 		System.out.println("Escolha um cliente abaixo [ID]: ");
 		ClientesCollection.listarClientes();
 		String client_id = Utils.scanner.next();
 		Utils.limparTela();
 		
-		//Obtendo a op��o de entrada
+		//Obtendo a opção de entrada
 		System.out.println("Escolha uma entrada. Caso n�o deseje uma entrada, digite 'N':");
 		System.out.println("ID | Descrição");
 		//Essa fun��o itera a cole��o de comidas e retorna apenas as do tipo "Entrada"
@@ -34,8 +60,13 @@ public class Pedido {
 				ComidasCollection.listarComidaNoPedido(i);
 			}		
 		}
-		String entrada_id = Utils.scanner.next();
-		Utils.limparTela();
+		
+		if (!Utils.scanner.next().equals("N")) {
+			String entrada_id = Utils.scanner.next();
+			construtor += 1;
+			Utils.limparTela();
+		}
+
 		
 		//Obtendo opção de prato-principal
 		System.out.println("Escolha um prato principal. Caso n�o deseje prato principal, digite 'N':");
@@ -46,8 +77,12 @@ public class Pedido {
 				ComidasCollection.listarComidaNoPedido(i);
 			}		
 		}
+		
+		if (!Utils.scanner.next().equals("N")) {
 		String principal_id = Utils.scanner.next();
+		construtor += 1;
 		Utils.limparTela();
+		}
 		
 		//Obtendo sobremesa
 		System.out.println("Escolha uma sobremesa. Caso n�o deseje sobremesa, digite 'N':");
@@ -58,8 +93,12 @@ public class Pedido {
 				ComidasCollection.listarComidaNoPedido(i);
 			}		
 		}
+		
+		if (!Utils.scanner.next().equals("N")) {
 		String sobremesa_id = Utils.scanner.next();
+		construtor += 1;
 		Utils.limparTela();
+		}
 		
 		//Obtendo bebida
 		System.out.println("Escolha uma bebida. Caso n�o deseje bebida, digite 'N':");
@@ -70,11 +109,18 @@ public class Pedido {
 				BebidasCollection.listarBebidaNoPedido(i);	
 		}
 		
+		if (!Utils.scanner.next().equals("N")) {
 		String bebida_id = Utils.scanner.next();
+		construtor += 1;
 		Utils.limparTela();
+		}
 		
 		System.out.println("Sumário do pedido:");
 		//Traz o pedido
+//		switch(construtor) {
+//		case 1: 
+//		}
+		
 		
 		System.out.println("Confirmar?");
 		
